260a7ce068cc6668773cc708298b06eac01b2b57
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2017-02-21 03:30:36 +0200

    Added ignore patterns for Jest. Added a very silly, but TDD-style assertions.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 /**
  * Proxifies the class or the method
- * @return {Function|Object} Proxified object
+ * @param  {Function|Object}  input   Function or object to proxify
+ * @return {Function|Object}          Proxified object
  */
-export default function proxify() {
-  return () => {};
+export default function proxify(input) {
+  if (input instanceof Object) {
+    return input;
+  } else if (input instanceof Function) {
+    return input;
+  }
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,15 @@
-import index from './';
+import proxify from './';
 
 describe('Main lib tests', () => {
   it('Is a defined function', () => {
-    expect(index).toBeInstanceOf(Function);
+    expect(proxify).toBeInstanceOf(Function);
+  });
+
+  it('Returns an object if supplied with object', () => {
+    expect(proxify({})).toBeInstanceOf(Object);
+  });
+
+  it('Returns a function if supplied with function', () => {
+    expect(proxify(() => {})).toBeInstanceOf(Function);
   });
 });
