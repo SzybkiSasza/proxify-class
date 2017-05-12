9d8be7e9fcc1d31484f5f672ef88c5044be51648
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2017-05-12 04:19:07 +0300

    Initial files structure.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
-/**
- * Proxifies the class or the method
- * @param  {Function|Object}  input   Function or object to proxify
- * @return {Function|Object}          Proxified object
- */
-export default function proxify(input) {
-  if (input instanceof Function) {
-    return input;
-  } else if (input instanceof Object) {
-    return input;
-  }
-}
+export * from './proxify-function';
+export * from './proxify-class';
