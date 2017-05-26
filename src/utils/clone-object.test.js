50236aef1215be70d5f1f18e7c645f472f3dbf12
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2017-05-26 04:47:43 +0300

    Added some tests for cloner.

--- src/utils/clone-object.test.js
+++ src/utils/clone-object.test.js
@@ -1,4 +1,4 @@
-/* eslint-disable require-jsdoc */
+/* eslint-disable require-jsdoc, no-invalid-this */
 
 import clone from './clone-object';
 
@@ -24,6 +24,42 @@ describe('Clone Object', () => {
     // We assume they are returned in the same order as on the original
     expect(Object.keys(clonedObj)).toEqual(['x', 'y', 'z']);
     expect(clonedObj.z).not.toBe(original.z);
+  });
+
+  it('By default clones every property', () => {
+    const original = {
+      x: 1,
+      y: () => 2,
+      z: function() {
+        return 3;
+      },
+    };
+
+    const clonedObj = clone(original);
+    original.z = function() {
+      return 5;
+    };
+    original.x = 123;
+
+    expect(clonedObj.x).toEqual(1);
+  });
+
+  it('Uses customizer for cloning function properties, if passed', () => {
+    const original = {
+      x: 1,
+      y: (val) => val + 2,
+    };
+
+    // Creates a function that will run original with value + 1
+    const customizer = (original) => (val) => original(val + 1);
+
+    const clonedObj = clone(original, customizer);
+
+    const originalResult = original.y(1);
+    const clonedResult = clonedObj.y(1);
+
+    expect(originalResult).toEqual(3);
+    expect(clonedResult).toEqual(4);
   });
 
   it('Clones the Class definition', () => {
@@ -128,5 +164,28 @@ describe('Clone Object', () => {
     const second = B.someMethod();
 
     expect(first).toEqual(second);
+  });
+
+  it('Uses customizer for cloning class methods, if passed', () => {
+    const Original = function() {
+      this.prop = 'val';
+    };
+    Original.prototype.method = function(variable) {
+      this.prop2 = variable + 1;
+      return this.prop2;
+    };
+
+    // Creates a function that will run original with value + 2
+    // Returns a bound function!
+    const customizer = (originalFunction) => function(variable) {
+      return originalFunction.bind(this)(variable + 2);
+    };
+
+    const ClonedClass = clone(Original, customizer);
+    const clonedObject = new ClonedClass();
+
+    const result = clonedObject.method(4); // 4 + 2 + 1 = 7
+    expect(result).toEqual(7);
+    expect(clonedObject.prop2).toEqual(7);
   });
 });
