51773010df787757c257b89e5288546c866ae7ff
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2017-05-26 05:20:00 +0300

    Added last unit tests. Working on component tests.

--- src/methods/proxify-class.test.js
+++ src/methods/proxify-class.test.js
@@ -1,3 +1,5 @@
+import {reduce} from 'lodash';
+
 jest.mock('../utils/clone-object');
 jest.mock('./proxify-function');
 
@@ -22,5 +24,70 @@ describe('Proxify Class', () => {
     clone.mockReturnValueOnce(input);
     const proxified = proxifyClass(input);
     expect(proxified).toBeInstanceOf(Object);
+  });
+
+  it('Proxifies all the functions if decide is not present', () => {
+    // Returns string - does not matter, we just want to check if it was run
+    proxifyFunction.mockReturnValue('SHOULD BE PROXIFIED FUNCTION HERE');
+
+    // Just a simple iteration over the properties and running cloner
+    clone.mockImplementation((obj, cloner) =>
+      reduce(obj, (memo, value, key) => {
+        if (value instanceof Function) {
+          memo[key] = cloner(value);
+        } else {
+          memo[key] = value;
+        }
+
+        return memo;
+      }, {})
+    );
+
+    const original = {
+      a: function() {},
+      b: function() {},
+      c: 123,
+    };
+
+    const clonedObject = proxifyClass(original, () => {});
+
+    expect(clonedObject).toEqual({
+      a: 'SHOULD BE PROXIFIED FUNCTION HERE',
+      b: 'SHOULD BE PROXIFIED FUNCTION HERE',
+      c: 123,
+    });
+  });
+
+  it('Proxifies only the functions that decide returns true for', () => {
+    // Returns string - does not matter, we just want to check if it was run
+    proxifyFunction.mockReturnValue('SHOULD BE PROXIFIED FUNCTION HERE');
+
+    // Just a simple iteration over the properties and running cloner
+    clone.mockImplementation((obj, cloner) =>
+      reduce(obj, (memo, value, key) => {
+        if (value instanceof Function) {
+          memo[key] = cloner(key, value);
+        } else {
+          memo[key] = value;
+        }
+
+        return memo;
+      }, {})
+    );
+
+    const original = {
+      a: function() {},
+      b: function() {},
+      c: 123,
+    };
+
+    const decide = (propertyName) => propertyName === 'a';
+    const clonedObject = proxifyClass(original, () => {}, decide);
+
+    expect(clonedObject).toEqual({
+      a: 'SHOULD BE PROXIFIED FUNCTION HERE',
+      b: expect.any(Function),
+      c: 123,
+    });
   });
 });
